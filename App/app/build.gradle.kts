a9b89dcec88e1f73c8e5b51668c559e0304a0bca
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-05-19 07:49:46 +0300

    added search bar + two floating buttons

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -69,4 +69,7 @@ dependencies {
 
     // material components
     implementation("com.google.android.material:material:1.9.0")
+
+    // swipe to refresh
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 }
