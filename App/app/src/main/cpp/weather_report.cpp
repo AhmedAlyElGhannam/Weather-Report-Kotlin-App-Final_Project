63eff0f5b43fce28e0a7966616075551d2273cb5
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-05-21 14:19:43 +0300

    added full cpp calculation functions

--- App/app/src/main/cpp/weather_report.cpp
+++ App/app/src/main/cpp/weather_report.cpp
@@ -7,49 +7,81 @@ extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_celciusToKelvin(
         JNIEnv *env, jobject thiz, jdouble temp) {
-    // TODO: implement celciusToKelvin()
+    jdouble res = 0.0;
+
+    res = (temp * (9.0/5.0)) + 32.0;
+
+    return res;
 }
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_celciusToFahrenheit(
         JNIEnv *env, jobject thiz, jdouble temp) {
-    // TODO: implement celciusToFahrenheit()
+    jdouble res = 0.0;
+
+    res = temp + 273.15;
+
+    return res;
 }
 /******************************SPEED********************************/
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_meterPerSecondToKilometerPerHour(
         JNIEnv *env, jobject thiz, jdouble wind_speed) {
-    // TODO: implement meterPerSecondToKilometerPerHour()
+    jdouble res = 0.0;
+
+    res = wind_speed * 3.6;
+
+    return res;
 }
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_meterPerSecondToMilePerHour(
         JNIEnv *env, jobject thiz, jdouble wind_speed) {
-    // TODO: implement meterPerSecondToMilePerHour()
+    jdouble res = 0.0;
+
+    res = wind_speed * 2.23694;
+
+    return res;
 }
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_meterPerSecondToFeetPerSecond(
         JNIEnv *env, jobject thiz, jdouble wind_speed) {
-    // TODO: implement meterPerSecondToFeetPerSecond()
+    jdouble res = 0.0;
+
+    res = wind_speed * 3.28084;
+
+    return res;
 }
 /******************************PRESSURE********************************/
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_hectopascalToPsi(
         JNIEnv *env, jobject thiz, jdouble pressure) {
-    // TODO: implement hectopascalToPsi()
+    jdouble res = 0.0;
+
+    res = pressure * 0.0145038;
+
+    return res;
 }
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_hectopascalToAtm(
         JNIEnv *env, jobject thiz, jdouble pressure) {
-    // TODO: implement hectopascalToAtm()
+    jdouble res = 0.0;
+
+    res = pressure / 1013.25;
+
+    return res;
 }
 extern "C"
 JNIEXPORT jdouble JNICALL
 Java_com_example_weather_1report_utils_UnitSystemsConversions_00024Companion_hectopascalToBar(
         JNIEnv *env, jobject thiz, jdouble pressure) {
-    // TODO: implement hectopascalToBar()
+    jdouble res = 0.0;
+
+    res = pressure / 1000;
+
+    return res;
 }
