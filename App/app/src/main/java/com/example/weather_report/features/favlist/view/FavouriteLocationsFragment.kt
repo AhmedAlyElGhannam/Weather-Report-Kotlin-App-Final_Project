5a55be1af6a3a95140ba80a92c85ce75d4b995b4
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-05-28 09:19:06 +0300

    added dialog to make the user second guess removing stuff from favs

--- App/app/src/main/java/com/example/weather_report/features/favlist/view/FavouriteLocationsFragment.kt
+++ App/app/src/main/java/com/example/weather_report/features/favlist/view/FavouriteLocationsFragment.kt
@@ -1,6 +1,11 @@
 package com.example.weather_report.features.favorites
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.location.Geocoder
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,7 +19,6 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weather_report.contracts.FavouriteLocationsContract
-import com.example.weather_report.utils.ISelectedCoordinatesOnMapCallback
 import com.example.weather_report.databinding.FragmentFavouritesBinding
 import com.example.weather_report.features.details.viewmodel.WeatherDetailsViewModel
 import com.example.weather_report.features.favlist.view.FavouriteLocationsAdapter
@@ -27,7 +31,9 @@ import com.example.weather_report.model.remote.IWeatherService
 import com.example.weather_report.model.remote.RetrofitHelper
 import com.example.weather_report.model.remote.WeatherAndForecastRemoteDataSourceImpl
 import com.example.weather_report.model.repository.WeatherRepositoryImpl
+import com.example.weather_report.utils.ISelectedCoordinatesOnMapCallback
 import java.util.Locale
+
 
 class FavouriteLocationsFragment
     : Fragment(),
@@ -59,7 +65,6 @@ class FavouriteLocationsFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setupRecyclerView()
         setupObservers()
         viewModel.loadFavouriteLocations()
@@ -68,7 +73,12 @@ class FavouriteLocationsFragment
 
     override fun onAddNewLocationClickListener() {
         binding.fabAdd.setOnClickListener {
-            MapDialog(this).show(parentFragmentManager, "MapDialog")
+            if (isConnectedToInternet()) {
+                MapDialog(this).show(parentFragmentManager, "MapDialog")
+            }
+            else {
+                Toast.makeText(requireContext(), "Please connect to the internet to add more locations", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -85,7 +95,16 @@ class FavouriteLocationsFragment
                 }
             },
             onRemoveClick = { locationId ->
-                viewModel.removeFavourite(locationId)
+                AlertDialog.Builder(context)
+                    .setTitle("Favourite Location")
+                    .setMessage("Are you sure you want to remove this location from favourites?")
+                    .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
+                        viewModel.removeFavourite(locationId)
+                        Toast.makeText(context, "Location Removed from Favourites", Toast.LENGTH_SHORT)
+                            .show()
+                    })
+                    .setNegativeButton("No", null)
+                    .show()
             }
         )
 
@@ -122,5 +141,16 @@ class FavouriteLocationsFragment
             lon,
             cityWithCountryCode
         )
+    }
+
+    private fun isConnectedToInternet(): Boolean {
+        val connectivityManager = activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.activeNetwork?.let {
+            connectivityManager.getNetworkCapabilities(it)
+        }
+        return networkCapabilities?.let {
+            it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+        } ?: false
     }
 }
